073c82a09080d048d093387b4609de5c5c19d7c8
Held Grijo <von.grijo@pmm.am.gov.br>
2018-10-05 16:07:52 -0400

    Tests configurations

--- index-test.php
+++ index-test.php
@@ -4,12 +4,16 @@
  * This file should be removed when the application is deployed for production.
  */
 
+defined('DS') or define('DS', DIRECTORY_SEPARATOR);
+
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../yii-1.1/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/test.php';
+$config = require __DIR__.DS.'protected'.DS.'config'.DS.'test.php';
 
 // remove the following line when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
+require_once __DIR__.DS.'vendor'.DS.'autoload.php';
 Yii::createWebApplication($config)->run();
